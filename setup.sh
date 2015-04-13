88a590b54cdaa40cf2cf16254919ac6b7dc1c554
Robert Spychala <robspychala@gmail.com>
2015-04-13 16:05:08 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
-echo "Setting up..."
+echo "Setting up directories..."
+
+ mkdir -p go/src
+ mkdir -p go/bin
+ mkdir -p go/pkg
+ 
+ mkdir -p go/src/app/server-http
+ mkdir -p go/src/app/server-worker
+ mkdir -p go/src/app/server-shared
+ mkdir -p go/src/app/server-template
+ mkdir -p go/src/app/client-assets
+
