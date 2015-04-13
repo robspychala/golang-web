52a2520ad7ede20d0fa9dc51b9d3181a042f4fb9
Robert Spychala <robspychala@gmail.com>
2015-04-13 17:06:37 -0400

    updated

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ mkdir -p go/src
 mkdir -p go/bin
 mkdir -p go/pkg
 
-curl -L https://github.com/robspychala/golang-web/archive/master.tar.gz | tar -xz app
+curl -L https://github.com/robspychala/golang-web/archive/master.tar.gz | tar -xvz -C go/src --strip-components 1 --include *./app
