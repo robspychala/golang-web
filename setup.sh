041790f88b9aa958c4fcda0169d4d696cfb28e77
Robert Spychala <robspychala@gmail.com>
2015-04-13 23:09:53 -0400

    added

--- setup.sh
+++ setup.sh
@@ -7,3 +7,5 @@ mkdir -p go/pkg
 curl -L https://github.com/robspychala/golang-web/archive/master.tar.gz | tar -xvz -C go/src --strip-components 1 --include *./app
 
 curl -L https://github.com/robspychala/golang-web/archive/master.tar.gz | tar -xvz --strip-components 1 --include *./Vagrantfile
+
+vagrant up
